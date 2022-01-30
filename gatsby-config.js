21123a570aec9952aa384d1d20896b7b31e12ea3
Puck Siemerink <pujapuhuk@gmail.com>
2022-01-30 20:22:52 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,10 @@ module.exports = {
       title: `My first Gatsby project`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
     resolve: 'gatsby-source-datocms',
     options: {
       "apiToken": "b1f0e42012b0613b7b8581b46aabee"
